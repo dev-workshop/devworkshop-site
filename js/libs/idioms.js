758db9907dbcb50ea52e6b8f6e807ab1f015a263
Scott Williams <scott.joe.williams@gmail.com>
2014-11-27 10:04:31 -0500

    fixed links, added idioms to other pages. moved idoms into custom jquery plugin

--- js/libs/idioms.js
+++ js/libs/idioms.js
@@ -1,70 +1,89 @@
-var idioms = [
-	{
-		"quote": "Today is the first day of the rest of your life.",
-		"author": "Charles Dederich"
-	},
-	{
-		"quote": "In times of change learners inherit the earth; while the learned find themselves beautifully equipped to deal with a world that no longer exists.",
-		"author": "Eric Hoffer"
-	},
-	{
-		"quote": "I have no special talent. I am only passionately curious.",
-		"author": "Albert Einstein"
-	},
-	{
-		"quote": "Strive not to be a success, but rather to be of value.",
-		"author": "Albert Einstein"
-	},
-	{
-		"quote": "Anyone who has never made a mistake has never tried anything new.",
-		"author": "Albert Einstein"
-	},
-	{
-		"quote": "Weakness of attitude becomes weakness of character.",
-		"author": "Albert Einstein"
-	},
-	{
-		"quote": "I have no special talent. I am only passionately curious.",
-		"author": "Albert Einstein"
-	},
-	{
-		"quote": "We cannot solve our problems with the same thinking we used when we created them.",
-		"author": "Albert Einstein"
-	},
-	{
-		"quote": "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.",
-		"author": "Albert Einstein"
-	},
-	{
-		"quote": "Only one who devotes himself to a cause with his whole strength and soul can be a true master. For this reason mastery demands all of a person.",
-		"author": "Albert Einstein"
-	},
-	{
-		"quote": "The future belongs to those who prepare for it today.",
-		"author": "Malcolm X"
-	},
-	{
-		"quote": "A year from now, you'll wish you had started today.",
-		"author": "Karen Lamb"
-	},
-	{
-		"quote": "Have Less. Do more. Be more.",
-		"author": ""
-	},
-	{
-		"quote": "The future belongs to the few of us still willing to get our hands dirty.",
-		"author": ""
-	},
-	{
-		"quote": "You can't stop the waves, but you can learn to surf.",
-		"author": ""
-	},
-	{
-		"quote": "I'm not telling you it's going to be easy, I'm telling you it's going to be worth it.",
-		"author": ""
-	},
-	{
-		"quote": "Be so good they can't ignore you.",
-		"author": "Steve Martin"
-	},
-];
+(function ( $ ) {
+	$.fn.idioms = function() {
+		var idioms = [
+			{
+				"quote": "Today is the first day of the rest of your life.",
+				"author": "Charles Dederich"
+			},
+			{
+				"quote": "In times of change learners inherit the earth; while the learned find themselves beautifully equipped to deal with a world that no longer exists.",
+				"author": "Eric Hoffer"
+			},
+			{
+				"quote": "I have no special talent. I am only passionately curious.",
+				"author": "Albert Einstein"
+			},
+			{
+				"quote": "Strive not to be a success, but rather to be of value.",
+				"author": "Albert Einstein"
+			},
+			{
+				"quote": "Anyone who has never made a mistake has never tried anything new.",
+				"author": "Albert Einstein"
+			},
+			{
+				"quote": "Weakness of attitude becomes weakness of character.",
+				"author": "Albert Einstein"
+			},
+			{
+				"quote": "I have no special talent. I am only passionately curious.",
+				"author": "Albert Einstein"
+			},
+			{
+				"quote": "We cannot solve our problems with the same thinking we used when we created them.",
+				"author": "Albert Einstein"
+			},
+			{
+				"quote": "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.",
+				"author": "Albert Einstein"
+			},
+			{
+				"quote": "Only one who devotes himself to a cause with his whole strength and soul can be a true master. For this reason mastery demands all of a person.",
+				"author": "Albert Einstein"
+			},
+			{
+				"quote": "The future belongs to those who prepare for it today.",
+				"author": "Malcolm X"
+			},
+			{
+				"quote": "A year from now, you'll wish you had started today.",
+				"author": "Karen Lamb"
+			},
+			{
+				"quote": "Have Less. Do more. Be more.",
+				"author": ""
+			},
+			{
+				"quote": "The future belongs to the few of us still willing to get our hands dirty.",
+				"author": ""
+			},
+			{
+				"quote": "You can't stop the waves, but you can learn to surf.",
+				"author": ""
+			},
+			{
+				"quote": "I'm not telling you it's going to be easy, I'm telling you it's going to be worth it.",
+				"author": ""
+			},
+			{
+				"quote": "Be so good they can't ignore you.",
+				"author": "Steve Martin"
+			}
+		];
+
+		var selected = idioms[$.getRandomInt(0, (idioms.length-1))];
+
+		if (selected.author !== '') {
+			selected.author = ' - ' + selected.author;
+		}
+
+		this.html(selected.quote+selected.author);
+
+		return this;
+	};
+
+	$.getRandomInt = function( min, max ) {
+		return Math.round(Math.random() * (max - min)) + min;
+	};
+
+}( jQuery ));
