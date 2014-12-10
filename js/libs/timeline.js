db9696f45bd448b108b8da1a20c256ece392e2b6
Scott Williams <scott.joe.williams@gmail.com>
2014-12-09 21:02:27 -0500

    finally hooked up the timeline plugin to the DOM and entered the event info provided by Todd Shelton

--- js/libs/timeline.js
+++ js/libs/timeline.js
@@ -32,58 +32,83 @@
 		};
 		var options = $.extend({}, defaults, options);
 		var now = new Date();
-		var start = new Date(options.start);
-		var end = new Date(options.end);
+		var now = Date.parse(now);
+		var start = Date.parse(options.start);
+		var end = Date.parse(options.end);
 		var duration = end - start;
 		var sumPercent = 0;
 
 		// step through all the deadlines
 		for (var i = 0; i <= options.deadlines.length - 1; i++) {
-			var curStartDate = new Date(options.deadlines[i].startDate);
+			var curDeadline = options.deadlines[i];
+			var curDeadlineStartDate = Date.parse(curDeadline.startDate);
 			
 			if (i == 0) {
 				// first
-				options.deadlines[i].duration = new Date(options.deadlines[i+1].startDate) - start;
+				curDeadline.duration = Date.parse(options.deadlines[i+1].startDate) - start;
 			} else if (i == options.deadlines.length - 1) {
 				// last
-				options.deadlines[i].duration = end - curStartDate;
+				curDeadline.duration = end - curDeadlineStartDate;
 			} else {
 				// middle
-				var nextStartDate = new Date(options.deadlines[i+1].startDate);
-				options.deadlines[i].duration = nextStartDate - curStartDate;
+				var nextStartDate = Date.parse(options.deadlines[i+1].startDate);
+				curDeadline.duration = nextStartDate - curDeadlineStartDate;
 			}
-			
+
 			// if the last figure comes out above 100, just make it the remainder
-			var percent = Math.round((options.deadlines[i].duration / duration) * 100);
-			options.deadlines[i].percent = ((sumPercent+percent)>100) ? (100 - sumPercent) : percent;
+			var percent = Math.round((curDeadline.duration / duration) * 100);
+			curDeadline.percent = ((sumPercent+percent)>100) ? (100 - sumPercent) : percent;
 			sumPercent += percent;
+
+			// progress-bar
+			// progress-bar-striped
+			// active
+			
+			// progress-bar-expired
+			// progress-bar-info
+			// progress-bar-success
+			// progress-bar-warning
+			// progress-bar-danger
+
+			if ((curDeadlineStartDate+curDeadline.duration) < now) {
+				// past deadline
+				var bar = $('<div>', {
+					'class': 'progress-bar progress-bar-expired progress-bar-striped',
+					'style': 'width: '+curDeadline.percent+'%'
+				}).append($('<span>', {
+					text: '$'+curDeadline.price
+				}));
+			} else if ( (curDeadlineStartDate < now) && ((curDeadlineStartDate+curDeadline.duration) > now) ) {
+				// current deadline
+				var burnPercent = Math.round(((now - curDeadlineStartDate) / curDeadline.duration) * 100);
+				var burnBar = $('<span>', {
+					'id': "ttl-burn",
+					'class': 'progress-bar progress-bar-expired progress-bar-striped active',
+					'style': 'width: '+burnPercent+'%'
+				});
+
+				var bar = $('<div>', {
+					'class': 'progress-bar progress-bar-'+curDeadline.color+' progress-bar-striped active',
+					'style': 'width: '+curDeadline.percent+'%'
+				}).append(burnBar).append($('<span>', {
+					text: '$'+curDeadline.price
+				}));
+
+				$('#currentPrice').text(curDeadline.price);
+			} else if (curDeadlineStartDate > now) {
+				// future deadline
+				var bar = $('<div>', {
+					'class': 'progress-bar progress-bar-'+curDeadline.color,
+					'style': 'width: '+curDeadline.percent+'%'
+				}).append($('<span>', {
+					text: '$'+curDeadline.price
+				}));
+			}
+
+			this.append(bar);
 		}
-
-		console.log(options.deadlines);
-
-		// set `expire` class on if past	
-		// find percentage of time remaining
-		// set width
-		// black out
-
-		// set current price
-		// percent through current deadline
-
-		// var selected = idioms[$.getRandomInt(0, (idioms.length-1))];
-
-		// if (selected.author !== '') {
-		// 	selected.author = ' - ' + selected.author;
-		// }
-
-		// this.html(selected.quote+selected.author);
-
-
 
 		return this;
 	};
-
-	// $.getRandomInt = function( min, max ) {
-	// 	return Math.round(Math.random() * (max - min)) + min;
-	// };
 
 }( jQuery ));
